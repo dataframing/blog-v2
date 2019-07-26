49588b8c06add1638ca4bc384bd5956c1639809c
Danny Vilela <dov205@nyu.edu>
2019-07-26 00:36:03 -0700

    Update generate_entry.py

--- generate_entry.py
+++ generate_entry.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python3
+import os
+import re
+import sys
+from datetime import datetime as dt
+from pathlib import Path
+from typing import NoReturn
 
-import sys, os, re
-from datetime import datetime
-
-TEMPLATE = """
+TEMPLATE: str = """
 Title: {title}
 Date: {year}-{month:02d}-{day:02d} {hour}:{minute:02d}
-Category: Dross
+Category: 
 Tags:
-Description: META
+Description:
 Status: Draft
 
 <section markdown="1">
@@ -17,19 +19,28 @@ Status: Draft
 """
 
 
-def generate_entry(title):
-    today = datetime.today()
-    slug = re.sub("[^a-zA-Z0-9\-]", "", title.lower().strip().replace(" ", "-"))
+def generate_entry(title: str) -> NoReturn:
+    """
 
-    # Will attempt to make the year's directory. Does not throw an error if it exists
-    os.makedirs("content/{}".format(today.year), exist_ok=True)
-    new_article = "content/{}/{}.md".format(today.year, slug)
+    Args:
+        title:
 
-    if os.path.exists(new_article):
-        print("Error. Article already exists. Not overwriting.")
+    Returns:
+
+    """
+
+    today: dt = dt.today()
+    slug: str = re.sub("[^a-zA-Z0-9\-]", "", title.lower().strip().replace(" ", "-"))
+
+    # Will attempt to make the year's directory. Does not throw an error if it exists.
+    Path(f"content/{today.year}").mkdir(exist_ok=True)
+    new_entry: str = f"content/{today.year}/{slug}.md"
+
+    if Path(new_entry).exists():
+        print(f"Error: entry [{Path(new_entry)}] already exists. Not overwriting!")
         return
 
-    t = TEMPLATE.strip().format(
+    content: str = TEMPLATE.strip().format(
         title=title,
         year=today.year,
         month=today.month,
@@ -38,9 +49,9 @@ def generate_entry(title):
         minute=today.minute,
         slug=slug,
     )
-    with open(new_article, "w") as w:
-        w.write(t)
-    print("Article ready for editing: " + new_article)
+    with open(new_entry, "w") as w:
+        w.write(content)
+    print(f"Article ready for editing: {Path(new_entry)}")
 
 
 if __name__ == "__main__":
